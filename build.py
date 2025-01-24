53c22f9e021184c38570b8e1cd3dc347f902d3bc
Vitaliy Rybalka <Vitaliy.Rybalka@funkwerk.com>
2025-01-24 21:48:15 +0100

    implement command line arguments for build.sh: interactive, wip, overlay

--- build.py
+++ build.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+import argparse
 import logging
 import subprocess
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Build and run Docker container for Playwright E2E tests.")
+    parser.add_argument('--wip', action='store_true', help='Run only scenarios with attribute @wip')
+    parser.add_argument('--interactive', action='store_true', help='Show the browser and allow interacting with Python breakpoints')
+    parser.add_argument('--overlay', type=int, default=0, help='Show Given/When/Then steps as text overlay on the page and in videos for the specified number of seconds')
+    args = parser.parse_args()
+
     BUILD_CONTEXT = "build/context"
     CONTAINER = "playwright-e2e"
     IMAGE = f"{CONTAINER}-local"
@@ -17,7 +24,19 @@ def main():
 
         print(f"Recreating and running container: {CONTAINER}")
         sh(f"docker container rm -f {CONTAINER} 2>/dev/null")
-        sh(f"docker run -t --network=host --name {CONTAINER} {IMAGE}")
+
+        env = ""
+        flags = "--network=host -t"
+        if args.wip:
+            env += " -e WIP=1"
+        if args.interactive:
+            env += " -e INTERACTIVE=1 -e DISPLAY"
+            flags += " -i"
+            sh("xhost +local:")
+        if args.overlay > 0:
+            env += f" -e OVERLAY={args.overlay}"
+
+        sh(f"docker run {flags} --name {CONTAINER} {env} {IMAGE}")
     except subprocess.CalledProcessError as e:
         print(e)
     finally:
@@ -29,10 +48,10 @@ def main():
         sh(f"docker container rm {CONTAINER}")
 
 
-def sh(command, shell=True):
+def sh(command):
     """Run a shell command. Pass stdout and stderr through. Raise RuntimeError if returncode != 0."""
     print(f'+ {command}')
-    result = subprocess.run(command, shell=shell, check=True, capture_output=False, text=True)
+    result = subprocess.run(command, shell=True, check=True, capture_output=False, text=True)
 
 
 if __name__ == "__main__":
